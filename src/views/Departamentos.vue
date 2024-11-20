5e25d52c54fbb6557758a1ded99f8d9250398d06
Alejandro Muñoz <alejandromunozf005@gmail.com>
2024-11-20 15:34:54 -0500

    Finish departamento

--- src/views/Departamentos.vue
+++ src/views/Departamentos.vue
@@ -1,69 +1,81 @@
 <template>
-    <div class="container">
+  <div class="container">
       <h1>Listado de Departamentos |
-        <button @click="newDepartamento()" class="btn btn-success mx-2">
-          <font-awesome-icon icon="plus" />
-        </button>
+          <button @click="newDepartamento()" class="btn btn-success mx-2">
+              <font-awesome-icon icon="plus" />
+          </button>
       </h1>
       <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Código</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Municipios</th>
-            <th scope="col">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(departamento, index) in departamentos" :key="index">
-            <th scope="row">{{ index + 1 }}</th>
-            <td>{{ departamento.depto_codi }}</td>
-            <td>{{ departamento.depto_nomb }}</td>
-            <td v-if="departamento.municipios && departamento.municipios.length > 0">
-              <ul>
-                <li v-for="municipio in departamento.municipios" :key="municipio.muni_codi">
-                  {{ municipio.muni_nomb }}
-                </li>
-              </ul>
-            </td>
-            <td v-else>
-              Sin municipios
-            </td>
-            <td>
-              <button @click="editDepartamento(departamento.depto_codi)" class="btn btn-warning mx-2">
-                <font-awesome-icon icon="pencil" />
-              </button>
-              <button @click="deleteDepartamento(departamento.depto_codi)" class="btn btn-danger mx-2">
-                <font-awesome-icon icon="trash" />
-              </button>
-            </td>
-          </tr>
-        </tbody>
+          <thead>
+              <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Código</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Acciones</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="(departamento, index) in departamentos" :key="index">
+                  <th scope="row">{{ index + 1 }}</th>
+                  <td>{{ departamento.depa_codi }}</td>
+                  <td>{{ departamento.depa_nomb }}</td>
+                  <td>
+                      <button @click="deleteDepartamento(departamento.depa_codi)" class="btn btn-danger mx-2">
+                          <font-awesome-icon icon="trash" />
+                      </button>
+                      <button @click="editDepartamento(departamento.depa_codi)" class="btn btn-warning mx-2">
+                          <font-awesome-icon icon="pencil" />
+                      </button>
+                  </td>
+              </tr>
+          </tbody>
       </table>
-    </div>
-  </template>
-  
-  <script>
+  </div>
+</template>
+
+<script>
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 export default {
+  name: 'Departamentos',
   data() {
-    return {
-      error: null
-    };
+      return {
+          departamentos: [],
+      };
   },
   methods: {
-    fetchData() {
-      axios.get('http://127.0.0.1:8000/api/departamentos')
-        .then(response => {
-          // Handle successful response
-        })
-        .catch(error => {
-          this.error = error;
-          console.error('Error fetching data:', error);
-        });
-    }
-  }
-}
+      deleteDepartamento(codigo) {
+          Swal.fire({
+              title: `¿Quieres eliminar el departamento con ID ${codigo}?`,
+              showCancelButton: true,
+              confirmButtonText: 'Eliminar',
+          }).then((result) => {
+              if (result.isConfirmed) {
+                  axios.delete(`http://127.0.0.1:8000/api/departamentos/${codigo}`).then((response) => {
+                      if (response.data.success) {
+                          Swal.fire('¡Eliminado!', '', 'success');
+                          this.departamentos = response.data.departamentos;
+                      }
+                  }).catch(() => {
+                      Swal.fire('Error', 'No se pudo eliminar el departamento.', 'error');
+                  });
+              }
+          });
+      },
+      editDepartamento(id) {
+          this.$router.push({ name: 'EditarDepartamento', params: { id } });
+      },
+      newDepartamento() {
+          this.$router.push({ name: 'NuevoDepartamento' });
+      },
+  },
+  mounted() {
+      axios.get('http://127.0.0.1:8000/api/departamentos').then((response) => {
+          this.departamentos = response.data.departamentos;
+      }).catch(() => {
+          Swal.fire('Error', 'No se pudo cargar la lista de departamentos.', 'error');
+      });
+  },
+};
 </script>
